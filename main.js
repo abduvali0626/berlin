6469a3c7fbdc0df270ee435f2260fe8db188eaf5
abduvali0626 <umarrrrrrrrrrrrrrr63@gmail.com>
2025-02-08 10:11:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+let body = document.getElementById("by");
+
+function press1() {
+  body.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Flag_of_Bavaria_%28lozengy%29.svg/640px-Flag_of_Bavaria_%28lozengy%29.svg.png)";
+}
+  
+
+function press2() {
+  body.style.backgroundImage = "url(Flag_of_Berlin.svg.png)";
+}
+
+function press3() {
+  body.style.backgroundImage = "url(images.png)";
+}
+
+function press4() {
+  body.style.backgroundImage = "url(Flag_of_Schleswig-Holstein.svg.png)";
+}
+
+
+function press5() {
+  body.style.backgroundImage = "url(Flag_of_Hesse.svg.png)";
+}
+
+function press6() {
+  body.style.backgroundImage = "url(Flag_of_Baden-Württemberg.svg.png)";
+}
+
+function press7() {
+  body.style.backgroundImage = "url(Flag_of_Thuringia.svg)";
+}
+
+function press8() {
+  body.style.backgroundImage = "url(Flag_of_Saxony.svg.png)";
+}
